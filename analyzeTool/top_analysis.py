b8195c2aa58420f2368b9d0df76bb9e372992217
Satoshi Yamashita <sympathia3104@gmail.com>
2020-08-24 13:49:00 +0900

    feat: excel file output to top log analysis

--- analyzeTool/top_analysis.py
+++ analyzeTool/top_analysis.py
@@ -1,6 +1,8 @@
 import sys
 import csv
+import openpyxl
 from typing import List, Dict
+from openpyxl.chart import LineChart, Reference, Series
 
 PID_INDEX = 0
 CPU_INDEX = 8
@@ -8,6 +10,29 @@ MEM_INDEX = 9
 COMMAND_INDEX = 11
 PID_VALUE_COLUMN_COUNT = 12
 FILTER_VALUE = 1.0
+RANK_TOP_UPPER_LIMIT = 10
+
+
+def write_excel_file(header, max_order_indexes, array2d):
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet.title = 'top_result'
+    sheet = wb['top_result']
+    for i in range(len(header)):
+        sheet.cell(row=1, column=i+1).value = header[i]
+    for row in range(len(array2d)):
+        for col in range(len(max_order_indexes)):
+            sheet.cell(row=row+2, column=col+1).value = array2d[row][max_order_indexes[col]]
+    wb.save('../output/top_result.xlsx')
+
+
+def write_csv_file(header: List[str], max_order_indexes: List[int], array2d: List[List[str]]):
+    with open('../output/top_result.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, lineterminator='\n')
+        writer.writerow(header)
+        # writer.writerows(array2d)
+        for row in range(len(array2d)):
+            writer.writerow([array2d[row][index] for index in max_order_indexes])
 
 
 def create_max_value_order(str_max_values: List[str]):
@@ -34,23 +59,18 @@ def create_max_value_per_pid(array2d: List[List[str]]):
 
 def pack_empty_str(pid_count: int, array2d: List[List[str]]):
     for record in array2d:
-        diff = pid_count - (len(record) - 1)  # array2d row : time + pids
+        diff = pid_count - (len(record) - 1)
         record.extend([''] * diff)
 
 
-def write_csv_file(pids: List[str], array2d: List[List[str]]):
+def write_file(pids: List[str], array2d: List[List[str]]):
     pack_empty_str(len(pids), array2d)
     max_values_per_pid: List[str] = create_max_value_per_pid(array2d)
     max_order_indexes: List[int] = create_max_value_order(max_values_per_pid)
-
     header = [''] + pids
     array2d.append(['MAX:'] + max_values_per_pid)
-    with open('../output/top.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, lineterminator='\n')
-        writer.writerow(header)
-        # writer.writerows(array2d)
-        for row in range(len(array2d)):
-            writer.writerow([array2d[row][index] for index in max_order_indexes])
+    write_csv_file(header, max_order_indexes, array2d)
+    write_excel_file(header, max_order_indexes, array2d)
 
 
 def add_time_and_value_array2d(time: str, pids: List[str], mem_dict: Dict, array2d: List[List[str]]):
@@ -60,7 +80,7 @@ def add_time_and_value_array2d(time: str, pids: List[str], mem_dict: Dict, array
     record[0] = time
     for pid_key in mem_dict.keys():
         record[pids.index(pid_key) + 1] = mem_dict[pid_key]
-    array2d.append(record)
+    array2d.append(record)  # array2d record : time + pids
 
 
 def analyze_pid_value_line(line: str, pids: List[str], mem_dict: Dict):
@@ -73,10 +93,8 @@ def analyze_pid_value_line(line: str, pids: List[str], mem_dict: Dict):
         mem_dict[pid] = mem
 
 
-def analyze_top_log_lines(lines: List[str]):
+def analyze_top_log_lines(lines: List[str], pids: List[str], time_mem_array2d: List[List[str]]):
     is_pid_value_block = False
-    pids: List[str] = []
-    time_mem_array2d: List[List[str]] = []
     mem_dict: Dict = {}
     time = ''
     for line in lines:
@@ -93,13 +111,15 @@ def analyze_top_log_lines(lines: List[str]):
         if is_pid_value_block and len(line_columns) == PID_VALUE_COLUMN_COUNT:
             analyze_pid_value_line(line, pids, mem_dict)
     add_time_and_value_array2d(time, pids, mem_dict, time_mem_array2d)
-    write_csv_file(pids, time_mem_array2d)
 
 
 def analyze_top_log(file_path: str):
+    pids: List[str] = []
+    time_mem_array2d: List[List[str]] = []
     with open(file_path, 'r') as f:
         lines = f.readlines()
-        analyze_top_log_lines(lines)
+        analyze_top_log_lines(lines, pids, time_mem_array2d)
+    write_csv_file(pids, time_mem_array2d)
 
 
 def main(args: List[str]):
