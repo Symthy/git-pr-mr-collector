bfba030e6418b9fd63120e2d5d6d08ceb5d23604
Satoshi Yamashita <sympathia3104@gmail.com>
2021-07-16 19:09:07 +0900

    refactor: get github pr review comments

---
+++ git_pr_collect/Readme.md
@@ -0,0 +1,11 @@
+# Github から PR一覧と 各PRのレビューコメント取得 ツール
+
+## initialize
+
+- conf/token を作成
+- tokenファイルに github access token を記載
+
+## output
+
+- outフォルダに出力
+
--- git_pr_collect/get_github_pr_review_comments.py
+++ git_pr_collect/get_github_pr_review_comments.py
@@ -1,3 +1,9 @@
+import csv
+import os
+import shutil
+from abc import ABC, abstractmethod
+from typing import List
+
 import requests as requests
 
 OWNER_NAME = "Symthy"
@@ -9,44 +15,154 @@ STATE_VAL = "all"  # open, all, close
 SORT_VAL = "created"  # updated, created, popularity, long-running
 DIRECTION_VAL = "desc"  # desc, asc
 
+FILTER_PR_CREATOR = [
 
-def build_header():
-    with open('./conf/token') as f:
-        github_token = f.read()
-    return {
-        'Accept': 'application/vnd.github.v3+json',
-        'Authorization': 'token {}'.format(github_token)
-    }
+]
+FILTER_REVIEW_COMMENTER = [
+
+]
 
 
-def build_get_pulls_api_url():
-    return GITHUB_GET_PULL_API + '?state=' + STATE_VAL
+class ICsvWritableDataConverter(ABC):
+    @abstractmethod
+    def convert_header(self) -> List:
+        pass
+
+    @abstractmethod
+    def convert_body(self) -> List:
+        pass
+
+
+def write_csv(file_title: str, data: ICsvWritableDataConverter):
+    file_name = file_title.translate(str.maketrans({'/': '_', ':': '_', ' ': '_'}))
+    with open('out/' + file_name + '.csv', mode='a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(data.convert_header())
+        writer.writerows(data.convert_body())
+
+
+class PullRequestDataList(ICsvWritableDataConverter):
+    class PullRequestData:
+        def __init__(self, json_data):
+            self.pr_num: int = json_data['number']
+            self.title: str = json_data['title']
+            self.create_user: str = json_data['user']['login']
+            self.review_comments_url: str = json_data['review_comments_url']
+            self.comments_url: str = json_data['comments_url']
+
+        @staticmethod
+        def convert_header():
+            return ['PR num', 'PR title', 'PR create user', 'review comments url', 'comments url']
+
+        def convert_array(self):
+            return [self.pr_num, self.title, self.create_user, self.review_comments_url, self.comments_url]
+
+    def __init__(self, json_array: List):
+        self.values = self.__convert(json_array)
+
+    @staticmethod
+    def __convert(json_array: List) -> List[PullRequestData]:
+        prs: List[PullRequestDataList.PullRequestData] \
+            = [PullRequestDataList.PullRequestData(json_data) for json_data in json_array]
+        if len(FILTER_PR_CREATOR) > 0:
+            prs = list(filter(lambda pr: pr.create_user in FILTER_PR_CREATOR, prs))
+        return prs
+
+    def convert_header(self) -> List:
+        return PullRequestDataList.PullRequestData.convert_header()
+
+    def convert_body(self) -> List[List]:
+        arrays: List[List] = list(map(lambda pr_data: pr_data.convert_array(), self.values))
+        return arrays
+
+    def write_csv(self, file_title):
+        write_csv(file_title, self)
+
+
+class PullRequestReviewCommentList(ICsvWritableDataConverter):
+    class PullRequestReviewComment:
+        def __init__(self, json_data):
+            self.id = json_data['pull_request_review_id']
+            self.reviewer = json_data['user']['login']
+            self.comment = json_data['body']
+            self.review_comment_url = json_data['url']
+            self.file_path = json_data['path']
+            self.diff_hunk = json_data['diff_hunk']
+
+        @staticmethod
+        def convert_header():
+            return ['review id', 'reviewer', 'comment', 'file name', 'review comment url', 'diff']
+
+        def convert_array(self):
+            return [self.id, self.reviewer, self.comment, self.file_path, self.review_comment_url, self.diff_hunk]
+
+    def __init__(self, json_array: List):
+        self.values = self.__convert(json_array)
+
+    @staticmethod
+    def __convert(json_array: List) -> List[PullRequestReviewComment]:
+        review_comments: List \
+            = [PullRequestReviewCommentList.PullRequestReviewComment(json_data) for json_data in json_array]
+        if len(FILTER_REVIEW_COMMENTER) > 0:
+            review_comments = list(filter(lambda pr: pr.create_user in FILTER_PR_CREATOR, review_comments))
+        return review_comments
+
+    def convert_header(self) -> List:
+        return PullRequestReviewCommentList.PullRequestReviewComment.convert_header()
+
+    def convert_body(self) -> List[List]:
+        arrays: List[List] = list(map(lambda rev_data: rev_data.convert_array(), self.values))
+        return arrays
+
+    def write_csv(self, file_title):
+        write_csv(file_title, self)
 
 
 def main():
-    headers = build_header()
-    api_url = build_get_pulls_api_url()
-    response = requests.get(api_url, headers=headers)
-    # print(response.json())
-    json_array = response.json()
-    review_comments_url_datasets = []
-    for data in json_array:
-        pr_num = data['number']
-        user_name = data['user']['login']
-        review_comments_url = data['review_comments_url']
-        review_comments_url_datasets.append({
-            'pr': pr_num,
-            'creator': user_name,
-            'url': review_comments_url
-        })
+    def build_header():
+        with open('./conf/token') as f:
+            github_token = f.read()
+        return {
+            'Accept': 'application/vnd.github.v3+json',
+            'Authorization': 'token {}'.format(github_token)
+        }
 
-    for dataset in review_comments_url_datasets:
-        print('=== PR : {} ==='.format(dataset['pr']))
-        comments_res = requests.get(dataset['url'], headers=headers)
-        print('comments:')
-        for comment_obj in comments_res.json():
-            print(comment_obj['body'])
-        print('===============')
+    def build_get_pulls_api_url(page_count):
+        return GITHUB_GET_PULL_API + '?state=' + STATE_VAL + f'&page={page_count}&per_page=100'
+
+    def execute_github_api(url):
+        headers = build_header()
+        response = requests.get(url, headers=headers)
+        return response.json()
+
+    if os.path.exists('out/'):
+        shutil.rmtree('out/')
+    os.makedirs('out/', exist_ok=True)
+
+    # get PR
+    is_get_pr_retry = True
+    pr_page_count = 1
+    while is_get_pr_retry:
+        api_url = build_get_pulls_api_url(pr_page_count)
+        pr_json_array = execute_github_api(api_url)
+        pr_data_list = PullRequestDataList(pr_json_array)
+        pr_data_list.write_csv('pr_list')
+
+        # get PR review comment
+        for pr_data in pr_data_list.values:
+            is_get_review_comment_retry = True
+            review_comment_page_count = 1
+            while is_get_review_comment_retry:
+                comments_json_array = execute_github_api(
+                    pr_data.review_comments_url + f'?page={review_comment_page_count}&per_page=100')
+                pr_review_comment_list = PullRequestReviewCommentList(comments_json_array)
+                pr_review_comment_list.write_csv(f'{pr_data.pr_num}-{pr_data.title}')
+
+                review_comment_page_count += 1
+                is_get_review_comment_retry = False if len(comments_json_array) < 100 else True
+
+        pr_page_count += 1
+        is_get_pr_retry = False if len(pr_json_array) < 100 else True
 
 
 main()
