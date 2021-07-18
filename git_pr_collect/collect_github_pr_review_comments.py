3da0b55cbfc0f41f7e354720f337c16bbfebc31c
Satoshi Yamashita <sympathia3104@gmail.com>
2021-07-19 08:31:44 +0900

    refactor: interface (collect github pr review comments)

--- git_pr_collect/collect_github_pr_review_comments.py
+++ git_pr_collect/collect_github_pr_review_comments.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import re
 import shutil
 from abc import ABC, abstractmethod
 from typing import List, Callable
@@ -8,7 +9,7 @@ import requests as requests
 import configparser
 
 # changeable values
-GET_TARGET_PR_STATE_VAL = 'open'  # open, all, close
+GET_TARGET_PR_STATE_VAL = 'all'  # open, all, close
 # GET_TARGET_PR_SORT_VAL = 'created'  # updated, created, popularity, long-running
 # GET_TARGET_PR_DIRECTION_VAL = 'desc'  # desc, asc
 
@@ -28,11 +29,7 @@ PR_REVIEW_COMMENT_TEMPLATE_FILE_NAME = 'pr_review_comment.j2'
 
 class ICsvWritableDataConverter(ABC):
     @abstractmethod
-    def convert_header(self) -> List:
-        pass
-
-    @abstractmethod
-    def convert_body(self) -> List:
+    def convert_array(self) -> List:
         pass
 
 
@@ -44,11 +41,7 @@ class ICsvWriter(ABC):
 
 class ICsvConvertAndWriter(ICsvWritableDataConverter, ICsvWriter):
     @abstractmethod
-    def convert_header(self) -> List:
-        pass
-
-    @abstractmethod
-    def convert_body(self) -> List:
+    def convert_array(self) -> List:
         pass
 
     @abstractmethod
@@ -79,7 +72,8 @@ class IMarkdownConvertAndWriter(IMarkdownWritableDataConverter, IMarkdownWriter)
 
 
 def convert_filesystem_path_name(file_path: str):
-    return file_path.translate(str.maketrans({'/': '_', ':': '_', ' ': '_'}))
+    replace_file_path = file_path.translate(str.maketrans({'/': '_', ':': '_', ' ': '_'}))
+    return re.sub(r'[\\/:*?"<>|]+', '', replace_file_path)  # replace string that can't use to windows file path
 
 
 def write_csv(file_name: str, data: ICsvConvertAndWriter):
@@ -87,8 +81,7 @@ def write_csv(file_name: str, data: ICsvConvertAndWriter):
     try:
         with open(OUTPUT_DIR_PATH + csv_file_name, mode='a', newline='', encoding='CP932', errors='ignore') as f:
             writer = csv.writer(f)
-            writer.writerow(data.convert_header())
-            writer.writerows(data.convert_body())
+            writer.writerows(data.convert_array())
     except Exception as e:
         print('  -> write file failure:', e)
         return
@@ -98,7 +91,7 @@ def write_csv(file_name: str, data: ICsvConvertAndWriter):
 def write_md(sub_dir_name: str, file_name: str, data: IMarkdownConvertAndWriter):
     md_file_name = convert_filesystem_path_name(file_name) + '.md'
     try:
-        env = Environment(loader=FileSystemLoader(searchpath=TEMPLATES_DIR_PATH, encoding='utf8'))
+        env = Environment(loader=FileSystemLoader(searchpath=TEMPLATES_DIR_PATH, encoding='utf-8'))
         template = env.get_template(PR_REVIEW_COMMENT_TEMPLATE_FILE_NAME)
         md_file_data = template.render(data.convert_json())
         with open(OUTPUT_DIR_PATH + f'{sub_dir_name}/{md_file_name}', mode='w', encoding='CP932', errors='ignore') as f:
@@ -120,7 +113,7 @@ def read_text_file(file_path: str):
 
 
 class PullRequestDataList(ICsvConvertAndWriter):
-    class PullRequestData:
+    class PullRequestData(ICsvWritableDataConverter):
         def __init__(self, json_data):
             self.__pr_num: str = str(json_data['number'])
             self.__title: str = json_data['title']
@@ -165,12 +158,10 @@ class PullRequestDataList(ICsvConvertAndWriter):
             prs = list(filter(lambda pr: pr.create_user in filter_pr_create_user_list, prs))
         return prs
 
-    def convert_header(self) -> List:
-        return PullRequestDataList.PullRequestData.convert_header()
-
-    def convert_body(self) -> List[List]:
-        arrays: List[List] = list(map(lambda pr_data: pr_data.convert_array(), self.values))
-        return arrays
+    def convert_array(self) -> List[List]:
+        headers: List[List] = [PullRequestDataList.PullRequestData.convert_header()]
+        bodies: List[List] = list(map(lambda pr_data: pr_data.convert_array(), self.values))
+        return headers + bodies
 
     def write_csv(self, file_name):
         if self.is_empty():
@@ -183,7 +174,7 @@ class PullRequestDataList(ICsvConvertAndWriter):
 
 
 class PullRequestReviewCommentList(ICsvConvertAndWriter, IMarkdownWriter):
-    class PullRequestReviewComment(IMarkdownConvertAndWriter):
+    class PullRequestReviewComment(ICsvWritableDataConverter, IMarkdownConvertAndWriter):
         def __init__(self, json_data, pr_data: PullRequestDataList.PullRequestData):
             self.__id = str(json_data['id'])
             self.__reviewer = json_data['user']['login']
@@ -239,12 +230,10 @@ class PullRequestReviewCommentList(ICsvConvertAndWriter, IMarkdownWriter):
                 filter(lambda comment: comment.reviewer in filter_pr_reviewer_list, pr_review_comments))
         return pr_review_comments
 
-    def convert_header(self) -> List:
-        return PullRequestReviewCommentList.PullRequestReviewComment.convert_header()
-
-    def convert_body(self) -> List[List]:
-        arrays: List[List] = list(map(lambda rev_data: rev_data.convert_array(), self.values))
-        return arrays
+    def convert_array(self) -> List[List]:
+        headers: List[List] = [PullRequestReviewCommentList.PullRequestReviewComment.convert_header()]
+        bodies: List[List] = list(map(lambda rev_data: rev_data.convert_array(), self.values))
+        return headers + bodies
 
     def write_csv(self, file_name):
         if self.is_empty():
